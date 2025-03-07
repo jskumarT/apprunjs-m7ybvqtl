727a224d93468bd633126eca08fa6ca0073b3df1
jskumarT <22318991+undefined@users.noreply.github.com>
2025-03-07 08:49:40 +0000

    First commit

---
+++ stories/index.stories.js
@@ -0,0 +1,11 @@
+import "../src/index.jsx";
+
+export default {
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const story1 = () => `<div id="root">
+  <my-counter></my-counter>
+</div>`;
